d3a9b2c0a43387cda70e97dcaf12265f3edf95d8
crotch-watch <naps_08_bay@icloud.com>
2025-03-17 13:27:42 +0000

    refac: shifted to EventEmitter

--- File-Handling/File-Command/app.js
+++ File-Handling/File-Command/app.js
@@ -5,6 +5,7 @@ const READ = "read"
 const WRITE = "write"
 const DELETE = "delete"
 const CREATE = "create"
+const CHANGE = "CHANGE"
 
 const FILE_PATH =
   "/home/dev/Workspace/node-core/File-Handling/File-Command/command.txt"
@@ -14,28 +15,31 @@ const main = async function () {
 
   const fileHandle = await open(FILE_PATH)
 
+  fileHandle.on(CHANGE, async () => {
+    const { size: instructionLength } = await fileHandle.stat()
+    const FROM_START = 0
+
+    const { buffer: content } = await fileHandle.read({
+      buffer: Buffer.allocUnsafe(instructionLength),
+      position: FROM_START,
+      length: instructionLength
+    })
+
+    const instruction = content.toString().trim()
+    const START = 0 // beginning of string
+
+    const indexAfterCmd = instruction.indexOf(" ", START)
+    const command = string.slice(START, indexAfterCmd)
+
+    if (command === WRITE) {
+      console.log("command - write")
+    }
+  })
+
   const watcher = watch(FILE_PATH)
 
   for await (const { eventType } of watcher) {
     if (eventType === "change") {
-      const { size: instructionLength } = await fileHandle.stat()
-      const FROM_START = 0
-
-      const { buffer: content } = await fileHandle.read({
-        buffer: Buffer.allocUnsafe(instructionLength),
-        position: FROM_START,
-        length: instructionLength
-      })
-
-      const instruction = content.toString().trim()
-      const START = 0 // beginning of string
-
-      const indexAfterCmd = instruction.indexOf(" ", START)
-      const command = string.slice(START, indexAfterCmd)
-
-      if (command === WRITE) {
-        console.log("command - write")
-      }
     }
   }
 }
