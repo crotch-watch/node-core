d64b5a119b674081b85aaa5e7167f9f8c9cc3a48
crotch-watch <naps_08_bay@icloud.com>
2025-03-18 12:01:44 +0000

    add: onError defaults to console.log

--- File-Handling/utils.js
+++ File-Handling/utils.js
@@ -1,6 +1,6 @@
 const { isAsyncFunction } = require("util/types")
 
-module.exports.handleError = function (callback, onError) {
+module.exports.handleError = function (callback, onError = console.log) {
   const async = isAsyncFunction(callback)
 
   if (async) return callback().catch(onError)
