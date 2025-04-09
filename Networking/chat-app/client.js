76a728fdc4e4f251e722350b1c3f9956285d6b35
crotch-watch <naps_08_bay@icloud.com>
2025-04-09 22:52:39 +0530

    add: readline stdin -> stdout

--- Networking/chat-app/client.js
+++ Networking/chat-app/client.js
@@ -1,11 +1,20 @@
-const net = require("node:net")
-const { HOST, PORT } = require("./constants")
+import net from "node:net"
+import readline from "node:readline/promises"
+
+import { HOST, PORT } from "./constants.js"
 
 const client = net.createConnection({
   host: HOST,
   port: PORT
 })
 
-client.on("connect", () => {
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+client.on("connect", async () => {
   console.log("Connected")
+  const message = await rl.question("enter a message > ")
+  client.write(message)
 })
