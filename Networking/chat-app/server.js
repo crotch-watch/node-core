332c378ef158ea3406efcbd76b402c7082f28248
crotch-watch <naps_08_bay@icloud.com>
2025-04-09 22:53:10 +0530

    add: server displays message

--- Networking/chat-app/server.js
+++ Networking/chat-app/server.js
@@ -5,7 +5,12 @@ const server = net.createServer()
 
 server.on("connection", (socket) => {
     console.log("new connection", socket.address())
+
+    socket.on("data", chunk => {
+        console.log(chunk.toString())
+    })
 })
+
 server.listen(PORT, HOST, () => {
   console.log("Listening on", HOST, PORT)
 })
