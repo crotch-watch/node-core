8bad70d63abfd8e002571558cda175e9b064c33f
crotch-watch <naps_08_bay@icloud.com>
2025-05-12 23:00:22 +0530

    add: receives data writes to fs

--- Networking/uploader/server.js
+++ Networking/uploader/server.js
@@ -1,48 +1,23 @@
 const fs = require("node:fs/promises")
 const net = require("node:net")
 
-const fill_resource = async () => {
-  const file_handle = await fs.open("./text.txt", "a")
+const server = net.createServer()
 
+server.on("connection", async (socket) => {
+  const file_handle = await fs.open("./storage/dest.txt", "w")
   const write_stream = file_handle.createWriteStream()
 
-  const ITER = 1e5
-  const { writableHighWaterMark } = write_stream
-  let accumulator = ""
-  let current_index = 0
-
-  const loop = (start) => {
-    for (let i = start; i < ITER; i++) {
-      if (accumulator.length > writableHighWaterMark) {
-        write_stream.write(accumulator)
-        accumulator = ""
-        break
-      }
-
-      accumulator += i
-
-      if (i === ITER - 1) {
-        write_stream.write(accumulator)
-        write_stream.end()
-      }
+  socket.on("data", (chunk) => {
+    if (!write_stream.write(chunk)) {
+      socket.pause()
     }
-  }
-
-  loop(current_index)
+  })
 
   write_stream.on("drain", () => {
-    loop(current_index)
+    socket.resume()
   })
+})
 
-  write_stream.on("end", () => {
-    console.log("write-stream has written all data")
-
-    file_handle.close()
-  })
-}
-
-const main = async () => {
-  await fill_resource()
-}
-
-void main()
+server.listen(1024, "::1", () => {
+  console.log("server listening@", server.address())
+})
